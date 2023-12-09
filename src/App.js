af7f99c72e6d42be8766e527255bdc44ef4b3ba3
ChandupaSH <chandupash107@gmail.com>
2023-12-09 12:50:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
+// import { useState } from 'react';
+// import QRReader from 'react-qr-scanner';
+// import { useMediaQuery } from '@mui/material'
+
+// function App() {
+//   const isSmall = useMediaQuery('(min-width: 600px )')
+//   const widthIs = isSmall? '50%':'100%'
+//   const [result, setResult] = useState(null);
+
+//   const handleScan = data => {
+//     if (data) {
+//       setResult(data);
+//     }
+//   };
+
+//   const handleError = (error) => {
+//     if (error) {
+//       console.log(error)
+//     }
+//   }
+
+//   return (
+//     <div >
+//       <div>code changed</div>
+//       {result? null 
+//       :
+//       <QRReader
+//         style={{width: widthIs}}
+//         delay={300}
+//         onError={handleError}
+//         onScan={handleScan}
+//         facingMode={0}
+//       />}
+//       {result && <p>QR code scanned: {result.text}</p>}
+//     </div>
+//   );
+// }
+// export default App
+
 import { useState } from 'react';
 import QRReader from 'react-qr-scanner';
-import { useMediaQuery } from '@mui/material'
+import { useMediaQuery } from '@mui/material';
 
 function App() {
-  const isSmall = useMediaQuery('(min-width: 600px )')
-  const widthIs = isSmall? '50%':'100%'
+  const isSmall = useMediaQuery('(min-width: 600px)');
+  const widthIs = isSmall ? '50%' : '100%';
   const [result, setResult] = useState(null);
+  const [facingMode, setFacingMode] = useState('environment');
 
-  const handleScan = data => {
+  const handleCameraChange = (event) => {
+    setFacingMode(event.target.value);
+  };
+
+  const handleScan = (data) => {
     if (data) {
       setResult(data);
     }
@@ -15,24 +59,52 @@ function App() {
 
   const handleError = (error) => {
     if (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   return (
-    <div >
-      <div>code changed</div>
-      {result? null 
-      :
-      <QRReader
-        style={{width: widthIs}}
-        delay={300}
-        onError={handleError}
-        onScan={handleScan}
-        facingMode={0}
-      />}
+    <div>
+      {!result && (
+        <>
+          <h2>Select Camera:</h2>
+          <div>
+            <label htmlFor="frontCamera">Front Camera</label>
+            <input
+              type="radio"
+              id="frontCamera"
+              name="camera"
+              value="user"
+              checked={facingMode === 'user'}
+              onChange={handleCameraChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="backCamera">Back Camera</label>
+            <input
+              type="radio"
+              id="backCamera"
+              name="camera"
+              value="environment"
+              checked={facingMode === 'environment'}
+              onChange={handleCameraChange}
+            />
+          </div>
+        </>
+      )}
+      {result ? null : (
+        <QRReader
+          style={{ width: widthIs }}
+          delay={300}
+          onError={handleError}
+          onScan={handleScan}
+          facingMode={facingMode}
+        />
+      )}
       {result && <p>QR code scanned: {result.text}</p>}
     </div>
   );
 }
-export default App
+
+export default App;
+
